24b007d92f0dd9900d3f1cbf563e7d1e05c9424a
Arthur <arthurhenriquemelo2005@gmail.com>
2026-06-11 15:53:13 -0300

    Exercicio básico sistema login

---
+++ SistemaLogin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class Prgram
+{
+    static void Main(string [] args)
+    {
+        const string login = "Arthur";
+        const string senha = "1234";
+
+        Console.WriteLine("Qual o login do usuário: ");
+        string loginUser = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Informe sua senha: ");
+        string senhaUser = Console.ReadLine() ?? "";
+
+        if(loginUser == login && senhaUser == senha)
+        {
+            Console.WriteLine("Acessso liberado");
+        }
+        else
+        {
+            Console.WriteLine("Acesso Negado");
+        }
+    }
+}
