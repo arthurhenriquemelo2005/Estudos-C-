ecfb128c6bb4e2dedbc9dac92a5c6f3823d255ff
Arthur <arthurhenriquemelo2005@gmail.com>
2026-06-13 23:20:13 -0300

    Atualizando exercicio do caixa de mercado

--- CaixaMercado/Mercado.cs
+++ CaixaMercado/Mercado.cs
@@ -1,32 +1,41 @@
 using System;
-    
-    class Mercado
+using System.Diagnostics;
+using System.Net;
+
+class Mercado
 {
     static void Main(string[] args)
     {
-       
-            decimal valorTotal = 0;
 
-            Console.WriteLine("Nome do produto: ");
-            string nomeProduto = (Console.ReadLine()??"").ToLower();
+        decimal valorTotal = 0;
 
-            Console.WriteLine("Preço: ");
-            decimal precoProduto = decimal.Parse(Console.ReadLine() ?? "");
+        Console.WriteLine("digite (sair) para sair do sistema se não quiser pressione enter");
+        string usuarioSair = Console.ReadLine();
+        if (usuarioSair == "sair")
+        {
+            Environment.Exit(0);
+        }
+        Console.Clear();
 
-            Console.WriteLine("Quantidade: ");
-            int quantidadeProduto = int.Parse(Console.ReadLine() ??"");
+        Console.WriteLine("Nome do produto: ");
+        string nomeProduto = (Console.ReadLine() ?? "").ToLower();
 
-            Console.WriteLine("digite (sair) para sair do sistema");
+        Console.WriteLine("Preço: ");
+        decimal precoProduto = decimal.Parse(Console.ReadLine() ?? "");
 
-            valorTotal += precoProduto * quantidadeProduto;
-           
-            Console.Clear();
+        Console.WriteLine("Quantidade: ");
+        int quantidadeProduto = int.Parse(Console.ReadLine() ?? "");
 
-            Console.WriteLine("=== COMPROVANTE DE VENDA ====");
-            Console.WriteLine($"Produto: {nomeProduto}");
-            Console.WriteLine($"Preço do produto: {precoProduto}");
-            Console.WriteLine($"{quantidadeProduto} unidades");
-            Console.WriteLine($"Preco total: {valorTotal}");
-            
+
+        valorTotal += precoProduto * quantidadeProduto;
+
+        Console.Clear();
+
+        Console.WriteLine("=== COMPROVANTE DE VENDA ====");
+        Console.WriteLine($"Produto: {nomeProduto}");
+        Console.WriteLine($"Preço do produto: {precoProduto}");
+        Console.WriteLine($"{quantidadeProduto} unidades");
+        Console.WriteLine($"Preco total: {valorTotal}");
+
     }
 }
